93c7b6a5e0d89da2de7e074a5f841b23215e9747
esteban.sanchez <estebans991@gmail.com>
2020-12-05 13:21:37 -0300

    added validations to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,17 @@
 class Client < ApplicationRecord
+  validates :email, :first_name, :last_name, :job, :country,
+            :address, :zip_code, :phone, :external_client_id, presence: true
   has_many :payments
+
+  PARAMS = [
+    :email,
+    :first_name,
+    :last_name,
+    :job,
+    :country,
+    :address,
+    :zip_code,
+    :phone,
+    :external_client_id
+  ].freeze
 end
