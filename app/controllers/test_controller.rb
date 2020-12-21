86890e6b88d0520706e169038eda38247367f55b
esteban.sanchez <estebans991@gmail.com>
2020-12-21 11:33:28 -0300

    remove test controller

--- app/controllers/test_controller.rb
+++
@@ -1,9 +0,0 @@
-# Test
-# frozen_string_literal: true
-
-class TestController < ApplicationController
-  # lalala
-  def index
-    render json: { "id": 1, "text": 'prueba' }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'test/index'
   namespace :api do
     namespace :v1 do
       get 'clients', to: 'clients#index'
